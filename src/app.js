bcd34eaa3d00cfa4066a7c9f41ee566a1c3ca1a5
miiguelcp <miiguelcp09@gmail.com>
2021-10-29 14:35:39 -0400

    New commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("Loaded excuse");
+  document.querySelector("#excuse").innerHTML = generateExcuse();
 };
+
+function generateExcuse() {
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoPosition = Math.floor(Math.random() * who.length);
+  let whatPosition = Math.floor(Math.random() * what.length);
+  let whenPosition = Math.floor(Math.random() * when.length);
+
+  return who[whoPosition] + " " + what[whatPosition] + " " + when[whenPosition];
+}
